18001a225e18607b1e3bf9473b388fa4ea10ccab
Ubuntu <ubuntu@ip-172-31-3-219.us-west-2.compute.internal>
2019-03-13 07:00:24 +0000

    use breakout

--- daylen_train.py
+++ daylen_train.py
@@ -8,8 +8,8 @@ TRAIN = True
 TEST = False
 TRAIN_TRANSFER = False
 DOUBLE_Q = False
-#ENV_NAME = 'BreakoutDeterministic-v4'
-ENV_NAME = 'PongDeterministic-v4'  
+ENV_NAME = 'BreakoutDeterministic-v4'
+#ENV_NAME = 'PongDeterministic-v4'  
 # You can increase the learning rate to 0.00025 in Pong for quicker results
 
 
